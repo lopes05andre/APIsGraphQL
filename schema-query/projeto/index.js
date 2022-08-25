28446a20e22ffddf9719108f432cb9e757804ae8
Andre Lopes <andre.05.lopes@gmail.com>
2022-08-25 20:08:07 -0300

    Objeto Usuário e método resolver

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,20 +1,51 @@
 const { ApolloServer, gql} = require('apollo-server')
 
 const typeDefs = gql`
+    # Criar um novo tipo Scalar para retornar uma data
+    scalar Date
+
+    # Definindo tipo para usuário
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # Pontos de entrada da API!
     type Query {
-        ola: String  
-        horaAtual: String      
+        ola: String!  
+        horaAtual: Date!
+        usuarioLogado: Usuario     
     }
 `
 //Resolvers retornam os dados
 const resolvers = {
+    //resolver para tratar o diferenciador de salario
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
+    },
     Query: {
         ola() {
             return "Bom dia!"
         },
-        horaAtual(){
+        horaAtual() {
             return `${new Date}`
+        },
+        usuarioLogado(obj) {
+            console.log(obj)
+            return {
+                id: 1,
+                nome: 'Ana da Web',
+                email: 'anadaweb@email.com',
+                idade: 23,
+                salario_real: 1234.56,
+                vip: true
+            }
         }
     }
 }
@@ -28,4 +59,4 @@ server.listen().then(({ url }) => {
     console.log(`Executando em ${url}`)
 })
 
-//Continua na aula 12. Tipos básicos do graphQL
+//Continua na aula 15. Desafio Produto
