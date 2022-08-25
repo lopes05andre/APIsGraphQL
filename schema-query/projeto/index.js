83f817ad7b524e96881460f603a98d233bd1ebd7
Andre Lopes <andre.05.lopes@gmail.com>
2022-08-25 11:19:03 -0300

    Retornar data e hora atual

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,1 +1,31 @@
-console.log("teste")
+const { ApolloServer, gql} = require('apollo-server')
+
+const typeDefs = gql`
+    # Pontos de entrada da API!
+    type Query {
+        ola: String  
+        horaAtual: String      
+    }
+`
+//Resolvers retornam os dados
+const resolvers = {
+    Query: {
+        ola() {
+            return "Bom dia!"
+        },
+        horaAtual(){
+            return `${new Date}`
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
+
+//Continua na aula 12. Tipos básicos do graphQL
